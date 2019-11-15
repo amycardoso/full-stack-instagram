d66d2bad53d68884f3aca58d071cf4b0e98905d2
amycardoso <alice.cardoso10@gmail.com>
2019-11-14 19:44:52 -0500

    Database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://semana:semana@cluster0-dvsvu.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+});
 
 app.get('/', (req, res) => {
     return res.send('Hello World');

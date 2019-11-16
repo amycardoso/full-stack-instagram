a07093a1cf9ab5c8b3e574b17568a945be1d1e56
amycardoso <alice.cardoso10@gmail.com>
2019-11-15 23:29:07 -0500

    Real time

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../services/api';
+import io from 'socket.io-client';
 
 import './Feed.css'
 
@@ -15,9 +16,30 @@ class Feed extends Component {
     };
 
     async componentDidMount() {
+        this.registerToSocket();
         const response = await api.get('posts');
 
         this.setState({ feed: response.data });
+    }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333');
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] });
+        });
+
+        socket.on('like', likedPost => {
+            this.setState({
+                feed: this.state.feed.map(post =>
+                    post._id === likedPost._id ? likedPost : post
+                )
+            });
+        });
+    }
+
+    handleLike = id => {
+        api.post(`/posts/${id}/like`);
     }
 
     render() {
@@ -36,7 +58,9 @@ class Feed extends Component {
                         <img src={`http://localhost:3333/files/${post.image}`} alt=""></img>
                         <footer>
                             <div className="actions">
-                                <img src={like} alt="" />
+                                <button type="button" onClick={() => this.handleLike(post._id)}>
+                                    <img src={like} alt="" />
+                                </button>
                                 <img src={comment} alt="" />
                                 <img src={send} alt="" />
                             </div>
